567bcd419e25d2de21432496be3814cb0e7cf77e
dragonman225 <zxcvb22217@gmail.com>
2019-10-17 18:27:01 +0800

    feat(tag): Make a tag clickable and has an index

--- src/render-index.js
+++ src/render-index.js
@@ -11,33 +11,47 @@ module.exports = {
 function renderIndex(task) {
   const siteMeta = task.siteMeta
   const templateProvider = task.templateProvider
-  const operations = task.operations
-  const plugins = task.plugins
+  // const operations = task.operations
+  // const plugins = task.plugins
 
   /** Run `beforeRender` plugins. */
-  log('Run beforeRender plugins on index')
-  if (operations.enablePlugin) {
-    plugins.forEach(plugin => {
-      if (typeof plugin.func === 'function')
-        plugin.func.call({
-          pageType: 'index',
-          context: {
-            siteMeta
-          },
-          options: plugin.options
-        })
-      else
-        log(`Plugin ${plugin.name} is in wrong format, skipped`)
-    })
-  }
+  // log('Run beforeRender plugins on index')
+  // if (operations.enablePlugin) {
+  //   plugins.forEach(plugin => {
+  //     if (typeof plugin.func === 'function')
+  //       plugin.func.call({
+  //         pageType: 'index',
+  //         context: {
+  //           siteMeta
+  //         },
+  //         options: plugin.options
+  //       })
+  //     else
+  //       log(`Plugin ${plugin.name} is in wrong format, skipped`)
+  //   })
+  // }
 
   const workDir = process.cwd()
   const outDir = path.join(workDir, 'public')
   const indexPath = path.join(outDir, 'index.html')
-  
+
   Sqrl.autoEscaping(false)
+
+  log('Rendering home page')
   const html = Sqrl.Render(templateProvider.get('index'), {
     siteMeta
   })
   fs.writeFileSync(indexPath, html, { encoding: 'utf-8' })
+
+  siteMeta.tagMap.forEach((pageMetas, tagVal) => {
+    log(`Rendering tag: ${tagVal}`)
+    const html = Sqrl.Render(templateProvider.get('tag'), {
+      siteMeta,
+      tag: {
+        value: tagVal,
+        pageMetas
+      }
+    })
+    fs.writeFileSync(`${outDir}/tag/${tagVal}.html`, html, { encoding: 'utf-8' })
+  })
 }
